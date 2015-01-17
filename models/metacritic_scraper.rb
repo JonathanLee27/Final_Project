dfe7e4b7ea605f378d3cbff21c9862e98a565d80
jonathanlee27 <jonathanllee27@gmail.com>
2015-01-17 17:54:13 -0500

    We added genres

--- models/metacritic_scraper.rb
+++ models/metacritic_scraper.rb
@@ -7,10 +7,43 @@ response = Unirest.post "https://byroredux-metacritic.p.mashape.com/find/game",
     "Accept" => "application/json"
   },
   parameters:{
-    "platform" => 1,
+    "platform" => 2,
     "retry" => 4,
-    "title" => "Fallout 3"
+    "title" => "halo 3"
   }
 
   
-  puts JSON.parse(response.raw_body)[]
+  puts JSON.parse(response.raw_body)["result"]
+
+
+
+
+  {
+  platforms: {
+    "1" => "PlayStation 3",
+    "2" => "Xbox 360",
+    "3" => "PC",
+    "4" => "DS",
+    "6" => "PlayStation 2",
+    "7" => "PSP",
+    "8" => "Wii",
+    "9" => "iPhone/iPad",
+    "10" => "PlayStation",
+    "11" => "Game Boy Advance",
+    "12" => "Xbox",
+    "13" => "GameCube",
+    "14" => "Nintendo 64",
+    "15" => "Dreamcast",
+    "16" => "3DS",
+    "67365" => "PlayStation Vita",
+    "68410" => "Wii U"
+  },
+  genres: {
+    "action" => "Action",
+    "adventure" => "Adventure",
+    "fighting" => "Fighting Games",
+    "first-person" => "First-Person Shooters",
+    "flight" => "Flight/Flying",
+    "party" => "Party",
+    "platformer" => "Platformer",
+    "puzzle" => "Puzzle"}
